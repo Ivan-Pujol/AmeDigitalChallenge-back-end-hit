c1096e9e7c14d503c46d284259ffd304a8b867ca
Ivan-Pujol <ivan.ivanovich@hotmail.com>
2022-04-23 20:52:24 -0300

    configurado o arquivo .env e iniciada a API com Express

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+const express = require('express');
+const server = express();
 require('dotenv').config();
+const port = process.env.PORT;
+
+server.use(express.json());
+
+server.get('/teste', (req, res) =>{
+    return res.json({message: 'You are now in the API root'});
+})
+
+server.listen(port, ()=>{
+    console.log(`Wellcome your server is up!, Listening on port: ${port}`);
+})
